09d41bd219d2e41d1e83e32e6145b7c3e37cccb3
Shawn M Moore <sartak@bestpractical.com>
2009-12-22 09:32:42 -0500

    First cut

---
+++ lib/Plack/App/Template/Declare.pm
@@ -0,0 +1,55 @@
+package Plack::App::Template::Declare;
+use strict;
+use warnings;
+use parent 'Plack::Component';
+use Plack::Util;
+use Template::Declare;
+
+use Plack::Util::Accessor qw(view args);
+
+sub should_handle {
+    my ($self, $path) = @_;
+    return Template::Declare->has_template($path);
+}
+
+sub call {
+    my $self = shift;
+    my $env  = shift;
+
+    my $path = $env->{PATH_INFO};
+
+    if (!$self->should_handle($path)) {
+        return $self->return_404;
+    }
+
+    return $self->serve_path($env, $path);
+}
+
+sub args_for_show {
+    my $self = shift;
+    my $env  = shift;
+
+    return @{ $self->args };
+}
+
+sub serve_path {
+    my $self = shift;
+    my $env  = shift;
+    my $path = shift;
+
+    my $body = Template::Declare->show_template($path, $self->args_for_show($env));
+
+    return [
+        200,
+        [],
+        [$body],
+    ];
+}
+
+sub return_404 {
+    my $self = shift;
+    return [404, ['Content-Type' => 'text/plain'], ['not found']];
+}
+
+1;
+
